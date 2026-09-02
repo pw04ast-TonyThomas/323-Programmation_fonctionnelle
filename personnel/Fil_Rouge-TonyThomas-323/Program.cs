3f19c68b452cb48fc7680304f6fe94c0a7c85e99
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-09-02 10:27:22 +0200

    feat(DataPoint): implements the use of the DataPoint class
    [5]

--- personnel/Fil_Rouge-TonyThomas-323/Program.cs
+++ personnel/Fil_Rouge-TonyThomas-323/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Fil_Rouge_TonyThomas_323
+﻿using DataSeries;
+
+namespace Fil_Rouge_TonyThomas_323
 {
     internal class Program
     {
@@ -6,25 +8,25 @@
         {
             Console.WriteLine("Hello, World!");
 
-            DataSeries.DataSeries<ValorantMatch> valorant = DataSeries.DataSeries<ValorantMatch>.From(new[]
+            DataSeries.DataSeries<DataPoint<ValorantMatch>> valorant = DataSeries.DataSeries<DataPoint<ValorantMatch>>.From(new[]
             {
-                new ValorantMatch("Léa", "Jett",  18, 6, 4, 8,  13, true),
-                new ValorantMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false),
-                new ValorantMatch("Léa", "Neon",  20, 7, 5,  9, 13, true),
+                new DataPoint<ValorantMatch>(DateTime.Now, new ValorantMatch("Léa", "Jett",  18, 6, 4, 8,  13, true)),
+                new DataPoint<ValorantMatch>(DateTime.Now, new ValorantMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false)),
+                new DataPoint<ValorantMatch>(DateTime.Now, new ValorantMatch("Léa", "Neon",  20, 7, 5,  9, 13, true)),
             });
 
-            DataSeries.DataSeries<LolMatch> lol = DataSeries.DataSeries<LolMatch>.From(new[]
+            DataSeries.DataSeries<DataPoint<LolMatch>> lol = DataSeries.DataSeries<DataPoint<LolMatch>>.From(new[]
             {
-                new LolMatch("Léa", "Jett",  18, 6, 4, 8,  13, true),
-                new LolMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false),
-                new LolMatch("Léa", "Neon",  20, 7, 5,  9, 13, true),
+                new DataPoint<LolMatch>(DateTime.Now, new LolMatch("Léa", "Jett",  18, 6, 4, 8,  13, true)),
+                new DataPoint<LolMatch>(DateTime.Now, new LolMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false)),
+                new DataPoint<LolMatch>(DateTime.Now, new LolMatch("Léa", "Neon",  20, 7, 5,  9, 13, true)),
             });
 
-            DataSeries.DataSeries<Cs2Match> cs2 = DataSeries.DataSeries<Cs2Match>.From(new[]
+            DataSeries.DataSeries<DataPoint<Cs2Match>> cs2 = DataSeries.DataSeries < DataPoint<Cs2Match>>.From(new[]
 {
-                new Cs2Match("Léa", "Jett",  "CT", 6, 4, 8,  13, true),
-                new Cs2Match("Léa", "Reyna", "CT", 8, 2, 11,  9, false),
-                new Cs2Match("Léa", "Neon",  "T", 7, 5,  9, 13, true),
+                new DataPoint<Cs2Match>(DateTime.Now, new Cs2Match("Léa", "Jett",  "CT", 6, 4, 8,  13, true)),
+                new DataPoint<Cs2Match>(DateTime.Now, new Cs2Match("Léa", "Reyna", "CT", 8, 2, 11,  9, false)),
+                new DataPoint<Cs2Match>(DateTime.Now, new Cs2Match("Léa", "Neon",  "T", 7, 5,  9, 13, true)),
             });
 
             Console.WriteLine(valorant.Count);

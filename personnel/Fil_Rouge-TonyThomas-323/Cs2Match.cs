e4844f40b7708db8e6f83c00ea09d95180d77488
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-09-02 10:21:17 +0200

    feat(DataSeries): Finishes generics
    [10]

---
+++ personnel/Fil_Rouge-TonyThomas-323/Cs2Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fil_Rouge_TonyThomas_323
+{
+    public class Cs2Match
+    {
+        public string Player { get; }
+        public string Map { get; }
+        public string StartSide { get; }  // côté joué en 1re mi-temps (CT ou T)
+        public int Kills { get; }
+        public int Deaths { get; }
+        public int Assists { get; }
+        public int Mvps { get; }
+        public bool Won { get; }
+
+        public Cs2Match(string player, string map, string startSide, int kills, int deaths, int assists, int mvps, bool won)
+        {
+            Player = player;
+            Map = map;
+            StartSide = startSide;
+            Kills = kills;
+            Deaths = deaths;
+            Assists = assists;
+            Mvps = mvps;
+            Won = won;
+        }
+    }
+}
--- personnel/Fil_Rouge-TonyThomas-323/Program.cs
+++ personnel/Fil_Rouge-TonyThomas-323/Program.cs
@@ -6,25 +6,30 @@
         {
             Console.WriteLine("Hello, World!");
 
-            ValorantMatch[] valorantMatches = new[]
+            DataSeries.DataSeries<ValorantMatch> valorant = DataSeries.DataSeries<ValorantMatch>.From(new[]
             {
                 new ValorantMatch("Léa", "Jett",  18, 6, 4, 8,  13, true),
                 new ValorantMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false),
                 new ValorantMatch("Léa", "Neon",  20, 7, 5,  9, 13, true),
-            };
+            });
 
-            LolMatch[] lolMatches = new[]
+            DataSeries.DataSeries<LolMatch> lol = DataSeries.DataSeries<LolMatch>.From(new[]
             {
                 new LolMatch("Léa", "Jett",  18, 6, 4, 8,  13, true),
                 new LolMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false),
                 new LolMatch("Léa", "Neon",  20, 7, 5,  9, 13, true),
-            };
+            });
 
-            DataSeries.DataSeries<ValorantMatch> valorantMatch = DataSeries.DataSeries<ValorantMatch>.From(valorantMatches);
-            DataSeries.DataSeries<LolMatch> lolMatch = DataSeries.DataSeries<LolMatch>.From(lolMatches);
+            DataSeries.DataSeries<Cs2Match> cs2 = DataSeries.DataSeries<Cs2Match>.From(new[]
+{
+                new Cs2Match("Léa", "Jett",  "CT", 6, 4, 8,  13, true),
+                new Cs2Match("Léa", "Reyna", "CT", 8, 2, 11,  9, false),
+                new Cs2Match("Léa", "Neon",  "T", 7, 5,  9, 13, true),
+            });
 
-            Console.WriteLine(valorantMatch.Count);
-            Console.WriteLine(lolMatch.Count);
+            Console.WriteLine(valorant.Count);
+            Console.WriteLine(lol.Count);
+            Console.WriteLine(cs2.Count);
         }
     }
 }
